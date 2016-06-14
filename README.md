dac66db6d486a30ff6638d20a64c48e32ba0d93e
Tuxity <kevin.darcel@gmail.com>
2016-06-13 23:58:58 -0500

    First try with an XMLRPC lib, it works but the code is ugly

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 rtorrent-php
 ===========
 
-[![GPL V3](http://img.shields.io/badge/license-GPL_V2-blue.svg?style=flat)](http://opensource.org/licenses/GPL-3.0)
+[![GPL V3](http://img.shields.io/badge/license-GPL_V3-blue.svg?style=flat)](http://opensource.org/licenses/GPL-3.0)
+
+
+[rTorrent XMLRPC Reference](https://github.com/mdevaev/emonoda/wiki/rTorrent-XMLRPC-Reference)
--- src/Rtorrent/Client.php
+++ src/Rtorrent/Client.php
@@ -2,9 +2,36 @@
 
 namespace Rtorrent;
 
+use \PhpXmlRpc\Client as XMLRPCClient;
+use \PhpXmlRpc\Request as XMLRPCRequest;
+use \PhpXmlRpc\Value as XMLRPCValue;
+
 class Client
 {
-    public function __construct()
+    private $XMLRPC_client;
+
+    public function __construct($addr)
     {
+        $this->XMLRPC_client = new XMLRPCClient($addr);
+        //$this->XMLRPC_client->setDebug(2);
+    }
+
+    public function getDownloads()
+    {
+        $params = new XMLRPCValue([
+            new XMLRPCValue(''), // view
+            new XMLRPCValue('d.get_base_filename='),
+            new XMLRPCValue('d.get_name='),
+            new XMLRPCValue('d.get_base_path='),
+            new XMLRPCValue('d.get_complete='),
+            new XMLRPCValue('d.get_hash='),
+            new XMLRPCValue('d.get_local_id='),
+            new XMLRPCValue('d.get_ratio=')
+        ], 'array');
+
+        $request = new XMLRPCRequest('d.multicall', [$params]);
+        $response = $this->XMLRPC_client->send($request);
+
+        return $response;
     }
 }
