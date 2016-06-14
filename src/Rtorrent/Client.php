3b09338bdcaf4dc83c07eef8ae45ac5599225ebb
Tuxity <kevin.darcel@gmail.com>
2016-06-14 13:19:11 -0500

    More work with the XMLRPC lib, it's more pretty

--- src/Rtorrent/Client.php
+++ src/Rtorrent/Client.php
@@ -4,34 +4,36 @@ namespace Rtorrent;
 
 use \PhpXmlRpc\Client as XMLRPCClient;
 use \PhpXmlRpc\Request as XMLRPCRequest;
-use \PhpXmlRpc\Value as XMLRPCValue;
+use \PhpXmlRpc\Encoder as XMLRPCEncoder;
 
 class Client
 {
     private $XMLRPC_client;
+    private $encoder;
 
     public function __construct($addr)
     {
         $this->XMLRPC_client = new XMLRPCClient($addr);
+        $this->encoder = new XMLRPCEncoder();
         //$this->XMLRPC_client->setDebug(2);
     }
 
     public function getDownloads()
     {
-        $params = new XMLRPCValue([
-            new XMLRPCValue(''), // view
-            new XMLRPCValue('d.get_base_filename='),
-            new XMLRPCValue('d.get_name='),
-            new XMLRPCValue('d.get_base_path='),
-            new XMLRPCValue('d.get_complete='),
-            new XMLRPCValue('d.get_hash='),
-            new XMLRPCValue('d.get_local_id='),
-            new XMLRPCValue('d.get_ratio=')
-        ], 'array');
+        $method = 'd.multicall';
+        $params = [
+            '', // view
+            'd.get_base_filename=',
+            'd.get_name=',
+            'd.get_base_path=',
+            'd.get_complete=',
+            'd.get_hash=',
+            'd.get_local_id=',
+            'd.get_ratio='
+        ];
 
-        $request = new XMLRPCRequest('d.multicall', [$params]);
-        $response = $this->XMLRPC_client->send($request);
+        $resp = $this->XMLRPC_client->send(new XMLRPCRequest($method, [ $this->encoder->encode($params) ]));
 
-        return $response;
+        return $this->encoder->decode($resp->value());
     }
 }
