5a16e31cfe3e2b19e4b7e2baf8a9614e1f1f78ba
Tuxity <kevin.darcel@gmail.com>
2016-06-18 18:15:48 -0500

    Work work work

--- src/Rtorrent/Client.php
+++ src/Rtorrent/Client.php
@@ -2,38 +2,90 @@
 
 namespace Rtorrent;
 
-use \PhpXmlRpc\Client as XMLRPCClient;
-use \PhpXmlRpc\Request as XMLRPCRequest;
-use \PhpXmlRpc\Encoder as XMLRPCEncoder;
-
 class Client
 {
     private $XMLRPC_client;
     private $encoder;
 
+    private function call($method, $params)
+    {
+        $request = new \PhpXmlRpc\Request($method, [ $this->encoder->encode($params) ]);
+        $response = $this->XMLRPC_client->send($request);
+
+        if ($response->faultCode()) {
+            return $response->faultString();
+        }
+        return $this->encoder->decode($response->value());
+    }
+
     public function __construct($addr)
     {
-        $this->XMLRPC_client = new XMLRPCClient($addr);
-        $this->encoder = new XMLRPCEncoder();
+        $this->XMLRPC_client = new \PhpXmlRpc\Client($addr);
+        $this->encoder = new \PhpXmlRpc\Encoder();
         //$this->XMLRPC_client->setDebug(2);
     }
 
-    public function getDownloads()
+    public function get()
     {
-        $method = 'd.multicall';
+
+    }
+
+    public function getAll()
+    {
         $params = [
             '', // view
-            'd.get_base_filename=',
-            'd.get_name=',
-            'd.get_base_path=',
-            'd.get_complete=',
             'd.get_hash=',
-            'd.get_local_id=',
-            'd.get_ratio='
+            'd.get_name=',
+            'd.get_size_bytes=',
+            'd.get_completed_bytes=',
+            'd.get_down_rate=',
+            'd.get_up_rate=',
+            'd.get_down_total=',
+            'd.get_up_total=',
+            'd.is_open=',
+            'd.is_active=',
+            'd.is_hash_checking=',
+            'd.get_ratio=',
+            'd.get_chunk_size=',
+            'd.get_size_chunks=',
+            'd.get_completed_chunks=',
+            'd.get_state_changed='
         ];
 
-        $resp = $this->XMLRPC_client->send(new XMLRPCRequest($method, [ $this->encoder->encode($params) ]));
+        return $this->call('d.multicall', $params);
+    }
 
-        return $this->encoder->decode($resp->value());
+    public function add($torrent, $directory)
+    {
+
+    }
+
+    public function delete($hash)
+    {
+        $this->stop($hash);
+        return $this->call('d.erase', $hash);
+    }
+
+    public function start($hash)
+    {
+        $this->call('d.open', $hash);
+        return $this->call('d.start', $hash);
+    }
+
+    public function stop($hash)
+    {
+        $resp = $this->call('d.stop', $hash);
+        $this->call('d.close', $hash);
+        return $resp;
+    }
+
+    public function pause($hash)
+    {
+        return $this->call('d.stop', $hash);
+    }
+
+    public function exists($hash)
+    {
+        return $this->call('d.get_name', $hash) ? true : false;
     }
 }
--- tests/get_downloads.php
+++ tests/get_downloads.php
@@ -3,4 +3,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $client = new Rtorrent\Client("https://user:passwd@mydomain.com/RPC");
-var_dump($client->getDownloads());
+var_dump($client->getAll());
+var_dump($client->exists('709C76AD72CD0DDD13A781E0FD27CDAE53668511'));
+var_dump($client->get('709C76AD72CD0DDD13A781E0FD27CDAE53668511'));
